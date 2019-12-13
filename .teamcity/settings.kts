39cc97b7c4fa6699c3d2b81adc4a244cca97417f
Dmytro Sychevsky <sych@pdffiller.com>
2019-12-13 10:31:20 +0200

    first commit as a code

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,13 +1,39 @@
-package TestRoot_SychTest.buildTypes
+#package TestRoot_SychTest.buildTypes
+#
+#import jetbrains.buildServer.configs.kotlin.v2018_2.*
+#
+#object SychTestProject : BuildType({
+#    templates(SychTest_DockerBuid)
+#    id = AbsoluteId("SychTestProject")
+#    name = "test project"
+#
+#    vcs {
+#        root(SychVcsRootId)
+#    }
+#})
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.Swabra
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.swabra
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot'
 
-object SychTestProject : BuildType({
+version = "2018.2"
+
+project {
+    vcsRoot(ProjectVcs)
+    buildType(Build)
+}
+
+object Build : BuildType({
+
     templates(SychTest_DockerBuid)
     id = AbsoluteId("SychTestProject")
     name = "test project"
-
-    vcs {
-        root(SychVcsRootId)
-    }
 })
+
+#object ProjectVcs : GitVcsRoot({
+#    name = "ProjectVcs"
+#    url = "https://github.com/spring-projects/spring-Project.git"
+#})
